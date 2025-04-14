c2c44ca82e3669eaa7aa78649a80de44ae69b364
unknown <taha_gadoo@yahoo.com>
2025-04-14 20:27:33 +0200

    vv

--- taha.php
+++ taha.php
@@ -1,7 +1,5 @@
 <?php
-<<<<<<< HEAD
-	Echo 'taha mohamed taha gadoo mohmaed';
-=======
-	Echo 'taha mohamed taha gadoo hussieno';
->>>>>>> 2d30a2306d0ab2420bc93dfc1548996fe6574699
+
+	Echo 'taha mohamed taha gadoo mohmaed hussieno';
+
 ?>
